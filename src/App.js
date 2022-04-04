56ccac45cd9e0409da41e38b15057f13a54a7210
Tamanna-akter <atamanna577@gmail.com>
2022-04-04 20:27:36 +0600

    image added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Reviews from './components/Reviews/Reviews';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './NotFound.css';
+import notFoundImg from '../../images/notFound.gif';
 
 const NotFound = () => {
     return (
-        <div>
-            <h5>no found</h5>
+        <div className="error-container">
+            <div>
+                <img className="error-img" src={notFoundImg} alt="" />
+            </div>
+            <div>
+                <h1>Error 404 - Page not found ... Opps!</h1>
+            </div>
+
         </div>
     );
 };
